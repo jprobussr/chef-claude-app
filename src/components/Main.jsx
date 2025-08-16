fa31b02c7e0430db51ec2ed21ed500fdfcac55ec
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2025-08-16 13:49:11 -0500

    Practice Mapping over ingredients

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,17 +2,33 @@ import React from 'react';
 import './Main.css';
 
 const Main = () => {
+  const ingredients = ['onion', 'turkey', 'pineapple'];
+
+  const ingredientsList = ingredients.map((ingredient) => (
+    <li key={Math.random()}>{ingredient}</li>
+  ));
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   const formData = new FormData(e.currentTarget);
+  //   const newIngredient = formData.get('ingredient');
+  //   ingredients.push(newIngredient);
+  //   console.log(ingredients)
+  // }
   return (
-    <main className='main'>
+    <main className="main">
       <form className="add-ingredient-form">
         <input
           className="ingredient-input"
           aria-label="Add Ingredient"
           type="text"
           placeholder="e.g oregano"
+          name="ingredient"
         />
         <button className="ingredient-btn">Add Ingredient</button>
       </form>
+
+      <ul>{ingredientsList}</ul>
     </main>
   );
 };
